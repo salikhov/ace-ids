f831cd523ec73d30695b2e8a50ffbc2a9d693d5b
Maxim Salikhov <msalihov@gmail.com>
2019-08-11 22:17:34 -0400

    Fetch METARs

--- src/components/weather-area.js
+++ src/components/weather-area.js
@@ -1,14 +1,30 @@
 import React from "react"
+import axios from "axios"
 
 class WeatherArea extends React.Component {
+  state = {
+    metar: "2156Z 30011KT 10SM FEW150 SCT200 29/15 A2988",
+  }
+  componentDidMount() {
+    this.fetchAndTrimMetar()
+  }
+  fetchAndTrimMetar() {
+    axios.get("http://metar.vatsim.net/metar.php?id=KSFO").then(response => {
+      const { data } = response
+      const noairport = data.substring(7)
+      const metar = noairport.substring(0, noairport.indexOf("RMK") - 1)
+      this.setState({ metar })
+    })
+  }
   render() {
+    const metar = this.state.metar
     return (
       <div
         style={{ color: `red`, fontSize: `1.8em` }}
         contentEditable={true}
         spellCheck={false}
       >
-        Y 2156Z 30011KT 10SM FEW150 SCT200 29/15 A2988
+        H {metar}
         <br />
         CVFP L28/D01R BIRDS ***RWY 1L CLSD*** NO TRANSITIONS
       </div>
